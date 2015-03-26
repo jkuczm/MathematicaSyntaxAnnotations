ac1e1ad1f01b43b2f1bd90f7bece7a8722522a02
jkuczm <Jakub.Kuczmarski@gmail.com>
2015-03-26 17:18:36 +0100

    Version bump: 0.1.2

--- PacletInfo.m
+++ PacletInfo.m
@@ -1,10 +1,10 @@
 (* Paclet Info File *)
 
-(* created 2015/03/06*)
+(* created 2015/03/26*)
 
 Paclet[
     Name -> "SyntaxAnnotations",
-    Version -> "0.1.1",
+    Version -> "0.1.2",
     MathematicaVersion -> "6+",
     Description -> "Annotate syntax elements at box level",
     Creator -> "Jakub Kuczmarski",
